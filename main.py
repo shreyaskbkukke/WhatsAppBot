fc6aac741b2c2a57b8432c820d12bcb1c3ea3e85
Shreyas KB <shreyaskb333@gmail.com>
2022-11-30 23:24:30 +0530

    Delta 4.1v

--- main.py
+++ main.py
@@ -5,16 +5,13 @@ import time
 import pyautogui as pg
 
 #*****************************************
+Images = True
+Addmore = True
+ImageName = "TECHUTSAV posters_merged.jpg"
 def message():
     "set the message need to send here"
     new_line()
-    pg.typewrite("I am Shreyas KB, IT Quiz Event Lead")
-    new_line()
-    new_line()
-    pg.typewrite("Congratulations. You have successfully registered for IT Quiz Event. for more communication regarding the event I request you to join this group.")
-    new_line()
-    pg.typewrite("If you are already joined the group please ignore this message or else join with this link https://chat.whatsapp.com/KAYuC6bLqa93vucj9S7z5G")
-    pg.typewrite("Have a grate day,")
+    pg.typewrite("This is the content of bot")
     #pg.typewrite("") #text out put
     #mail.google.com  #open gmail
 #*****************************************
@@ -44,23 +41,63 @@ def new_line():
 
 def watermark():
     "to set bot watermark on the end of message"
-    pg.typewrite("_(bot message no need to replay)_")
-    pg.typewrite("                    -Bot(4.1v)🤖")
+    new_line()
+    pg.typewrite("_(this is bot automated message no need to replay)_")
+    new_line()
+    pg.typewrite("                                  -Bot(4.1v) 🤖 ")
 
+def img_click(image):
+    if pg.locateOnScreen(image, confidence=0.8)!= None:
+        pic = pg.locateOnScreen(image, confidence=0.8)
+        x1 = pic.left
+        y1 = pic.top
+        print("I can see "+image+" in "+str(x1)+" "+str(y1))
+        pg.moveTo(x1 + 28, y1 + 15)
+        time.sleep(2)
+        pg.click()
+        print("Click completed...!")
+        time.sleep(3)
+    else:
+        print("image "+image+" is not found")
+        time.sleep(1)
+
+def add_img(image):
+    print("looking for target icon")
+    img_click('BPpin.png')
+    img_click('BPfile.png')
+    pg.click()
+    time.sleep(4)
+    if pg.locateOnScreen('file.png', confidence=0.8) != None:
+        pg.typewrite(image)
+    pg.typewrite(["enter"])
+    time.sleep(5)
+
+def additional(n):
+    print("looking for target icon")
+    img_click('Add.png')
+    time.sleep(3)
+
+    pg.typewrite(n)
+    time.sleep(1)
+    pg.typewrite(["enter"])
+
+count = 1
 # to read the the message from csv file
-data = pd.read_csv("leads.csv")
+data = pd.read_csv("victim.csv")
 data_dict = data.to_dict('list')
-leads = data_dict['PhNumber']
+number = data_dict['PhNumber']
 messages = data_dict['Message']
-combo = zip(leads, messages)
+combo = zip(number, messages)
+print("Data is fetching...")
 
 # start spaming the message one by one
-for leads, messages in combo:
-    web.open("https://api.whatsapp.com/send?phone="+leads+"&text="+messages)
+for number, messages in combo:
+    print("starting messages sequence")
+    web.open("https://api.whatsapp.com/send?phone="+number+"&text="+messages)
     time.sleep(10)
 
     #added for two times inorder to remove run time error
-    web.open("https://api.whatsapp.com/send?phone="+leads+"&text="+messages)
+    web.open("https://api.whatsapp.com/send?phone="+number+"&text="+messages)
     time.sleep(8)
 
     # take cursure at the end of the line
@@ -70,6 +107,13 @@ for leads, messages in combo:
     #start the content of message
     message()
     watermark()
+    if Images == True:
+        add_img(ImageName)
+    if Addmore == True:
+        additional(str(count)+".pdf")
+    time.sleep(3)
+    if pg.locateOnScreen('End.png', confidence=0.8) != None:
+        img_click('send.png')
 
     #send the message
 #    pg.typewrite(["enter"])
@@ -84,3 +128,4 @@ for leads, messages in combo:
     tab_close()
     time.sleep(5)
     #final sleep of iteration
+    count = count + 1
